6bc2c4dcd596a86ba2c39f3866f267b5d94ab185
Marty <marty.stepien@gmail.com>
2017-07-12 20:19:53 +0100

    footer component created, data service provider moved to app module from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Todo } from './todo';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [TodoDataService]
+  providers: [],
 })
 export class AppComponent {
 
